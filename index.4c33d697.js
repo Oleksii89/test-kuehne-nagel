08f4ee7c2e6aab836a5536b07a6016c9925becdb
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-09-09 13:35:37 +0000

    Deploying to gh-pages from @ Oleksii89/test-kuhne-nagel@25558be5111089214e47f650df93c08e457e254b 🚀

---
+++ index.4c33d697.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".backdrop"),o=document.querySelector(".shipments");console.dir(o),console.dir(e),o.addEventListener("click",(e=>{console.log(e.target)}));
+//# sourceMappingURL=index.4c33d697.js.map
---
+++ index.ec6ee784.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".backdrop"),o=document.querySelector(".shipments");console.dir(o),console.dir(e),o.addEventListener("click",(function(e){console.log(e.target)}))}();
+//# sourceMappingURL=index.ec6ee784.js.map
