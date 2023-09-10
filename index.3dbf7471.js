513d3e414993ec5dfdbca9a35cfe2d5a0cf54717
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-09-10 13:54:50 +0000

    Deploying to gh-pages from @ Oleksii89/test-kuhne-nagel@53f604ec4f8d5bd0fa88d8199ea2c1f4f249805f 🚀

--- index.3dbf7471.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".backdrop"),t=document.querySelectorAll(".details");document.querySelector(".shipments-table");function c(){e.classList.remove("is-hidden")}t.forEach((function(e){e.addEventListener("click",c)})),e.addEventListener("click",(function(t){t.target===e&&e.classList.add("is-hidden")}))}();
-//# sourceMappingURL=index.3dbf7471.js.map
