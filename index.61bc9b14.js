fe9dedee37a33ae9dd01fc64490895b64f1f451f
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-09-10 10:59:07 +0000

    Deploying to gh-pages from @ Oleksii89/test-kuhne-nagel@51418f7fb9b4bf318c38c7f62fc9bbe173394ded 🚀

---
+++ index.61bc9b14.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".backdrop"),t=document.querySelectorAll(".details");document.querySelector(".shipments-table");function c(){e.classList.remove("is-hidden")}t.forEach((function(e){e.addEventListener("click",c)})),e.addEventListener("click",(t=>{t.target===e&&e.classList.add("is-hidden")}));
+//# sourceMappingURL=index.61bc9b14.js.map
--- index.ec6ee784.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".backdrop"),o=document.querySelector(".shipments");console.dir(o),console.dir(e),o.addEventListener("click",(function(e){console.log(e.target)}))}();
-//# sourceMappingURL=index.ec6ee784.js.map
